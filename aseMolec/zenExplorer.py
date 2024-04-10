5aba8306655a4a028f5b1c977a2e6f48236c0c03
Ioan-Bogdan Magdau <nim92@ioan-mac.ncl.ac.uk>
2024-04-10 18:22:59 +0100

    tinkering with some data structures for describing the datasets in the Zenodo community

--- aseMolec/zenExplorer.py
+++ aseMolec/zenExplorer.py
@@ -35,3 +35,32 @@ class ze:
     def cache_all_data(self):
         for url_key in tqdm(self.urls):
             self.get_zip(url_key)
+
+
+class AtomicConfigs:
+
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def plot(self):
+        # to be implemented: compositions by cluster size, volumes for periodic
+        pass
+
+class TrainData:
+
+    def __init__(self, atomic_configs, ab_initio_level, ab_initio_code, rec, zip, file):
+        self.atomic_configs = atomic_configs
+        self.ab_initio_level = ab_initio_level
+        self.ab_initio_code = ab_initio_code
+        self.rec = rec
+        self.zip = zip
+        self.file = file
+
+    def to_dict(self):
+        d = self.__dict__.copy()
+        d['atomic_configs'] = self.atomic_configs.name
+        return d
+
+    def __repr__(self):
+        return self.to_dict().__repr__()
